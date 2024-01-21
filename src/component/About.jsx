83627235f1de67f60875454318a86cec5bc2fca1
Md Salahuddin Khan <cisrony@gmail.com>
2024-01-21 21:33:06 +0600

    Commit for how to write Ternary Operator in JSX to our React Project

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,10 +1,14 @@
 const About = () => {
+  let marks = 90;
   return (
     <div>
       <h1> Hello 1</h1>
-      <h1> Hello {5 + 7}</h1>
-      <h1> Hello 3</h1>
-      <h1> Hello 4 - {new Date().getTime()}</h1>
+      <h4> Hello {5 + 7}</h4>
+      <h4> Hello 3</h4>
+      <h4> Hello 4 - {new Date().getTime()}</h4>
+
+      <h1>Ternary Operator</h1>
+      {marks > 80 ? <h3>Brilliant Result</h3> : <h3>Average Result</h3>}
     </div>
   );
 };
